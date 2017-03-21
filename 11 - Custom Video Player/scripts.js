bb5ba93679efab35220faebcf3e8fb1ad42ce741
Adrian S MUresan <adriansmuresan@gmail.com>
2017-03-20 21:24:29 -0500

    Add a handleRangeUpdate function with event listeners for the range of the volume and playbackrate range sliders

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,7 +6,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build our functions */
 function togglePlay() {
@@ -30,6 +30,10 @@ function skip() {
   console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
 }
+
+function handleRangeUpdate() {
+  console.log(this.value);
+}
 /* Hook up the event listeners */
 
 video.addEventListener('click', togglePlay);
@@ -38,4 +42,7 @@ toggle.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
