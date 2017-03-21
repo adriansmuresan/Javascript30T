1030adad9af7e424e94b2a8f5397f12fca520f52
Adrian S MUresan <adriansmuresan@gmail.com>
2017-03-20 20:41:59 -0500

    Add a togglePlay function that plays/pauses the video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -9,5 +9,15 @@ const skipButton = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('player__slider');
 
 /* Build our functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+
+  // if(video.paused) {
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
+}
 
 /* Hook up the event listeners */
