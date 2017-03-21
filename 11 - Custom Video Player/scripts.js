a3faf6a2e140bcc9425093fb163b382848cc7e6a
Adrian S MUresan <adriansmuresan@gmail.com>
2017-03-20 21:08:48 -0500

    Add a skip function that skips back and forward once we press those buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButton = player.querySelectorAll('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('player__slider');
 
 /* Build our functions */
@@ -25,6 +25,11 @@ function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   toggle.textContent = icon;
 }
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 /* Hook up the event listeners */
 
 video.addEventListener('click', togglePlay);
@@ -32,3 +37,5 @@ toggle.addEventListener('click', togglePlay);
 
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
