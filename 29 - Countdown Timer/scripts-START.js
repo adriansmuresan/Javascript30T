57c18fa781a1eddd9be3a5c71b40cdcc8398269d
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-18 16:06:10 -0500

    Declare setInterval and  calculate the secondsLeft in our countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,16 @@
 function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
-	console.log({now, then});
+	//console.log({now, then});
+
+	setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		// check if we should stop it! and from going lower than 0
+		if (setInterval <= 0) {
+			return;
+		}
+		// display teh interval
+		console.log(secondsLeft);
+
+	}, 1000);
 }
