653236ef604078795f132525d41114a8289b7307
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-18 23:42:01 -0500

    Assign our setInterval to a global constant, countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,15 +1,18 @@
+let countdown;
+
 function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	//console.log({now, then});
 
-	setInterval(() => {
+	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		// check if we should stop it! and from going lower than 0
 		if (setInterval <= 0) {
+			clearInterval(countdown);
 			return;
 		}
-		// display teh interval
+		// display the interval
 		console.log(secondsLeft);
 
 	}, 1000);
