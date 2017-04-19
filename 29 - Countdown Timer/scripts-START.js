12be33e01d51a278ee6f5e5c24804fefe99df9fe
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-19 00:23:21 -0500

    Adjust the hour to be displayed in canadian/usa style without 2 numbers and minutes to show a zero in front if we have just a single decimal

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -34,6 +34,7 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
 	const end = new Date(timestamp);
 	const hour = end.getHours();
+	const adjustedHour = hour > 12 ? hour -12 : hour;
 	const minutes = end.getMinutes();
-	endTime.textContent = `Be back in ${hour}:${minutes}`
+	endTime.textContent = `Be back in ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`
 }
