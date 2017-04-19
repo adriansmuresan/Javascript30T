cfe1438a7a7c5edd2dd7d3230a84656073d6ff27
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-18 23:56:04 -0500

    Declare a displayTimeLeft function and calculate the minutes and seconds left for our countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,16 +4,23 @@ function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	//console.log({now, then});
+	displayTimeLeft(seconds);
 
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		// check if we should stop it! and from going lower than 0
-		if (setInterval <= 0) {
+		if (secondsLeft <= 0) {
 			clearInterval(countdown);
 			return;
 		}
 		// display the interval
-		console.log(secondsLeft);
+		displayTimeLeft(secondsLeft);
 
 	}, 1000);
+}
+
+function displayTimeLeft(seconds) {
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	console.log({minutes, remainderSeconds});
 }
