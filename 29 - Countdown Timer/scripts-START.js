1b67087eeb69c94ef341ddb6dd8e495a83c418af
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-19 00:44:52 -0500

    Select the buttons from the page, add an event listener on click and call a tart function for each

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
 	const now = Date.now();
@@ -36,5 +37,11 @@ function displayEndTime(timestamp) {
 	const hour = end.getHours();
 	const adjustedHour = hour > 12 ? hour -12 : hour;
 	const minutes = end.getMinutes();
-	endTime.textContent = `Be back in ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`
+	endTime.textContent = `Be back in ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+	console.log(this);s
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
