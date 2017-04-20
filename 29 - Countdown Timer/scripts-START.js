6ff95a28417c869ecb1b044da9eb6345ee267888
Adrian S MUresan <adriansmuresan@gmail.com>
2017-04-19 23:50:41 -0500

    Add an event listener to our input form on submit and call a function that will take the inputed minutes and call the timer function

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,49 +4,57 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-	// clear any existing timers
-	clearInterval(countdown);
-	
-	const now = Date.now();
-	const then = now + seconds * 1000;
-	//console.log({now, then});
-	displayTimeLeft(seconds);
-	displayEndTime(then);
+    // clear any existing timers
+    clearInterval(countdown);
 
-	countdown = setInterval(() => {
-		const secondsLeft = Math.round((then - Date.now()) / 1000);
-		// check if we should stop it! and from going lower than 0
-		if (secondsLeft <= 0) {
-			clearInterval(countdown);
-			return;
-		}
-		// display the interval
-		displayTimeLeft(secondsLeft);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    //console.log({now, then});
+    displayTimeLeft(seconds);
+    displayEndTime(then);
 
-	}, 1000);
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it! and from going lower than 0
+        if (secondsLeft <= 0) {
+            clearInterval(countdown);
+            return;
+        }
+        // display the interval
+        displayTimeLeft(secondsLeft);
+
+    }, 1000);
 }
 
 function displayTimeLeft(seconds) {
-	const minutes = Math.floor(seconds / 60);
-	const remainderSeconds = seconds % 60;
-	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-	timerDisplay.textContent = display;
-	document.title = display;
-	//console.log({minutes, remainderSeconds});
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+    //console.log({minutes, remainderSeconds});
 }
 
 function displayEndTime(timestamp) {
-	const end = new Date(timestamp);
-	const hour = end.getHours();
-	const adjustedHour = hour > 12 ? hour -12 : hour;
-	const minutes = end.getMinutes();
-	endTime.textContent = `Be back in ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back in ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
 function startTimer() {
-	const seconds = parseInt(this.dataset.time);
-	//console.log(seconds);
-	timer(seconds);
+    const seconds = parseInt(this.dataset.time);
+    //console.log(seconds);
+    timer(seconds);
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    //console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
